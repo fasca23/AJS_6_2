1c87847453b14eac1ab5fa0236db4d9ed304ddb2
Vladimir Kamenev <fasca22@gmail.com>
2023-08-08 11:50:59 +0300

    check_1

--- src/js/table.js
+++ src/js/table.js
@@ -1,21 +1,11 @@
 export default function destructuring(obj) {
   const result = [];
   const { special } = obj;
-  // Проверяем есть ли вообще special
   if (!special) {
     return result;
   }
-  for (let i = 0; i < special.length; i += 1) {
-    // Если нет описания - так и пишем о нем
-    if (!special[i].description) {
-      special[i].description = 'Описание недоступно';
-    }
-    result.push({
-      id: special[i].id,
-      name: special[i].name,
-      icon: special[i].icon,
-      description: special[i].description,
-    });
-  }
-  return result;
+  return special.map((el) => ({
+    ...el,
+    description: el.description || 'Описание недоступно',
+  }));
 }
